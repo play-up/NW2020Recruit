c94087aee290234664621dcb818d7ecb82ac46ea
PsychicLee <1169150227@qq.com>
2020-02-17 09:37:47 +0800

    测试

---
+++ NW20Recruit/src/components/frontend/frontend.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="frontend"></div>
+</template>
+
+<script>
+export default {
+  name: "frontend",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.frontend {
+  background-image: url()
+}
+</style>
