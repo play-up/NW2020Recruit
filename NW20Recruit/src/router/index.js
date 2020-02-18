6452fe8bc29efde0df6db52c9d4bdb3525013856
kaolalala <1921346727@qq.com>
2020-02-18 23:29:29 +0800

    做了文字的动效

--- NW20Recruit/src/router/index.js
+++ NW20Recruit/src/router/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import StarryHome from "@/views/StarryHome";
 import design from '@/views/design'
-import Boolean from '@/components/content/Boolean'
-import sBoolean from '@/components/content/sBoolean'
 
 Vue.use(Router)
 
@@ -16,16 +14,7 @@ export default new Router({
     }, {
       path: "/design",
       name: "design",
-      component: design,
-      children: [{
-        path: 'boolean',
-        name: "Boolean",
-        component: Boolean
-      }, {
-        path: 'sBoolean',
-        name: 'sBoolean',
-        component: sBoolean
-      }]
+      component: design
     }
   ]
 });
