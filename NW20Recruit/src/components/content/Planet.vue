53c4b895a0cadef4d56fba36664af6abdacc6835
PsychicLee <1169150227@qq.com>
2020-02-20 19:58:20 +0800

    后台页面完成

--- NW20Recruit/src/components/content/Planet.vue
+++ NW20Recruit/src/components/content/Planet.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="container">
-    <img class="planet" src="~assets/planet.png" alt="星球" />
+  <div class="Planet">
+    <div class="bigPlanet-box">
+      <div class="bigPlanet"></div>
+    </div>
+    <div class="orbit">
+      <div class="smallPlanet-box">
+        <div class="smallPlanet"></div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
+<script scoped>
 export default {
   name: "Planet"
 };
 </script>
 
 <style scoped>
-.planet {
+.Planet {
+  transform-style: preserve-3d;
+}
+.bigPlanet-box {
   height: 480px;
   width: 480px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+}
+.bigPlanet {
+  height: 100%;
+  width: 100%;
+  background-image: url(~assets/bigPlanet.png);
+  background-position: center center;
+  background-size: cover;
+}
+.orbit {
+  height: 450px;
+  width: 450px;
+  position: relative;
+  border: 6px solid rgba(145, 184, 184, 0.5);
+  border-radius: 50%;
+  transform-style: preserve-3d;
+  animation: orbitRotate 3s linear 0s normal none infinite;
+}
+.smallPlanet-box {
+  height: 30px;
+  width: 30px;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform-style: preserve-3d;
+  transform: translateX(-50%) translateY(50%) rotateX(-90deg) rotateY(0deg);
+}
+.smallPlanet {
+  height: 100%;
+  width: 100%;
+  background-image: url(~assets/smallPlanet.png);
+  background-position: center center;
+  background-size: cover;
+  animation: planetrotate 3s linear 0s normal none infinite;
+}
+@keyframes orbitRotate {
+  from {
+    transform: rotateX(75deg) rotateY(25deg) rotateZ(0deg);
+  }
+
+  to {
+    transform: rotateX(75deg) rotateY(25deg) rotateZ(-360deg);
+  }
+}
+@keyframes planetrotate {
+  from {
+    transform: rotateY(0deg);
+  }
+
+  to {
+    transform: rotateY(-360deg);
+  }
 }
 </style>
--- NW20Recruit/src/views/Backend.vue
+++ NW20Recruit/src/views/Backend.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="backend">
-    <div class="title" ref="title">后台</div>
-    <div class="planet">
-      <planet></planet>
-    </div>
-    <transition name="fade">
-      <div id="content">
+    <div class="container">
+      <div class="title">
         <p
-          class="letter"
-          v-for="(letter, index) in letters"
+          v-for="(letter, index) in letters_title"
+          :class="{'letters_title_1': index % 2 == 0,'letters_title_2': index % 2 != 0}"
           :style="`animation-delay:${index * 0.05}s`"
           :key="index"
         >{{letter}}</p>
       </div>
-    </transition>
+      <div class="planet">
+        <planet></planet>
+      </div>
+      <div id="content">
+        <p
+          v-for="(letter, index) in letters_content"
+          :class="{'letters_content_1': index % 2 == 0,'letters_content_2': index % 2 != 0}"
+          :style="`animation-delay:${index * 0.05 + 1.5}s`"
+          :key="index"
+        >{{letter}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,18 +33,21 @@ export default {
   },
   data() {
     return {
+      title: "后台",
       content:
         "后台组致力于使用java语言，专注于主流先进框架的学习与应用。致力于Web后端API开发。Java就是轮子多，在这里我们将要掌握多种“轮子”的使用方法，提高代码性能，搭建更加合理的架构。同时我们也会学习如何与服务器交互。",
-      letters: []
+      letters_title: [],
+      letters_content: []
     };
   },
   methods: {
-    splitTexts() {
-      this.letters = this.content.split("");
+    splitTexts(value) {
+      return value.split("");
     }
   },
   mounted() {
-    this.splitTexts();
+    this.letters_title = this.splitTexts(this.title);
+    this.letters_content = this.splitTexts(this.content);
   }
 };
 </script>
@@ -52,17 +62,23 @@ export default {
   background-position: center center;
   position: relative;
 }
+.container {
+  position: relative;
+  top: 9%;
+  transform: translateY(50%);
+}
 .title {
   font-size: 96px;
   font-family: "微软雅黑";
   color: white;
   position: absolute;
-  top: 326px;
+  /* top: 326px; */
+  top: 205px;
   left: 60px;
 }
 .planet {
   position: absolute;
-  top: 121px;
+  /* top: 121px; */
   right: 14px;
 }
 #content {
@@ -72,17 +88,63 @@ export default {
   color: white;
   line-height: 50px;
   position: absolute;
-  top: 600px;
-  right: 30px;
+  /* top: 600px; */
+  top: 480px;
+  right: 60px;
 }
-.letter {
+.letters_content_1 {
   display: inline-block;
-  animation: landIn 0.5s ease-out both;
+  animation: landInTop_content 0.5s ease-out both;
 }
-@keyframes landIn {
+.letters_content_2 {
+  display: inline-block;
+  animation: landInBottom_content 0.5s ease-out both;
+}
+@keyframes landInTop_content {
   from {
     opacity: 0;
     transform: translateX(80%) translateY(-40%);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0) translateY(0);
+  }
+}
+@keyframes landInBottom_content {
+  from {
+    opacity: 0;
+    transform: translateX(80%) translateY(40%);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0) translateY(0);
+  }
+}
+.letters_title_1 {
+  display: inline-block;
+  animation: landInTop_title 1s ease-out both;
+}
+.letters_title_2 {
+  display: inline-block;
+  animation: landInBottom_title 1s ease-out both;
+}
+@keyframes landInTop_title {
+  from {
+    opacity: 0;
+    transform: translateX(-200%) translateY(-100%);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0) translateY(0);
+  }
+}
+@keyframes landInBottom_title {
+  from {
+    opacity: 0;
+    transform: translateX(200%) translateY(100%);
   }
 
   to {
